bbc50e15134282023c577ab735477ae55825a19b
AkhileshNegi <akhileshnegi.an3@gmail.com>
2020-09-15 23:24:55 +0530

    removing build from git ignore

---
+++ build/precache-manifest.a7e150306df99ddbf67c0e3aba6b5ada.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: '1bd3c1827ac7f5e3c703cfb90f25bc94',
+    url: './index.html'
+  },
+  {
+    revision: '818077a64a680ab58ac3',
+    url: './static/css/main.f96f9de3.chunk.css'
+  },
+  {
+    revision: '7334007951909dcb6caa',
+    url: './static/js/2.4505b663.chunk.js'
+  },
+  {
+    revision: 'fbf73d1ed7e41d48ebcb53ec4d810884',
+    url: './static/js/2.4505b663.chunk.js.LICENSE.txt'
+  },
+  {
+    revision: 'e7ddafde7e8d0442e46b',
+    url: './static/js/3.8f10ede1.chunk.js'
+  },
+  {
+    revision: '818077a64a680ab58ac3',
+    url: './static/js/main.a14619a2.chunk.js'
+  },
+  {
+    revision: 'b08464c5c2bdd077a1ce',
+    url: './static/js/runtime-main.b2d3cc7b.js'
+  },
+  {
+    revision: '9e6245520bfe139052f69101e02296b1',
+    url: './static/media/floweditor.9e624552.woff'
+  },
+  {
+    revision: 'cd6a1374dab8144ad05b8996c2bde8d6',
+    url: './static/media/floweditor.cd6a1374.ttf'
+  },
+  {
+    revision: 'e05f20946ebc43f57abf8e0b38cb3f02',
+    url: './static/media/floweditor.e05f2094.svg'
+  },
+  {
+    revision: '5e547148e6fcbe72b9949c4597566831',
+    url: './static/media/map.5e547148.jpg'
+  }
+]);
